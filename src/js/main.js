33b5806bca67b6b798e991a1a5e5eb2080979b71
Kirby Banman <kirby.banman@gmail.com>
2019-02-07 22:51:14 -0700

    destructure, validate, and format bug names

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,38 @@
 let sampleTemperature = 0.6;
+const bugNameMatcher = /([A-z ]+)\|([A-z ]+)/g;
 
-const bugName = document.getElementById("bug-name");
+const commonName = document.getElementById("common-name");
+const latinName = document.getElementById("latin-name");
 const button = document.getElementById("button");
 
-bugName.textContent = sampleNetwork(sampleTemperature).sentence;
 
-button.onclick = () => {
-  bugName.textContent = sampleNetwork(sampleTemperature).sentence;
+let getBugName = (sampleTemperature) => {
+  let bugNameRaw = sampleNetwork(sampleTemperature).sentence
+  let bugNameMatches = bugNameMatcher.exec(bugNameRaw);
+
+  while (bugNameMatches == null || bugNameMatches[1].trim() === "" || bugNameMatches[2].trim() === "") {
+    bugNameRaw = sampleNetwork(sampleTemperature).sentence
+    bugNameMatches = bugNameMatcher.exec(bugNameRaw);
+  }
+
+  return {commonName: bugNameMatches[1].trim(), latinName: bugNameMatches[2].trim()}
 }
 
-console.log("Hi.  :)")
+let populateBugName = (sampleTemperature) => {
+  let bugName = getBugName(sampleTemperature);
+
+  commonName.textContent = bugName.commonName;
+  latinName.textContent = bugName.latinName;
+}
+
+populateBugName(sampleTemperature);
+
+button.onclick = () => {
+  populateBugName(sampleTemperature);
+}
+
+console.log("Hi.");
+console.log("");
+console.log("I hope you're having a good day.")
+console.log("");
+console.log(":-)");
